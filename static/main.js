021838e8398bb8c4d2db6a4f6a7cb6477add10a9
Garri Proshian <proshian.itmo@gmail.com>
2024-07-04 02:02:03 +0300

    move swipe_emitter to a separate script

--- static/main.js
+++ static/main.js
@@ -1,4 +1,5 @@
 import gnameToGrid from './gridname_to_grid.js';
+import {SwipeEmitter} from './swipe_emitter.js';
 
 function fill_keyboard(keyboardElem, keyboardData) {
     const keyboardRect = keyboardElem.getBoundingClientRect();
@@ -54,99 +55,6 @@ function empty_keyboard(keyboardElem) {
 
 
 
-class SwipeEmiter extends EventTarget{
-    constructor(keyboard_el, width, height) {
-        super();
-        this.keyboard_el = keyboard_el;
-        this.grid_width = width
-        this.grid_height = height
-        this.touchPositions = { x: [], y: [], t: [] };
-
-        keyboard_el.addEventListener('touchstart', this.handleTouchStart.bind(this));
-        keyboard_el.addEventListener('touchmove', this.handleTouchMove.bind(this));
-        keyboard_el.addEventListener('touchend', this.handleTouchEnd.bind(this));
-        keyboard_el.addEventListener('mousedown', this.handleMouseDown.bind(this));
-        keyboard_el.addEventListener('mousemove', this.handleMouseMove.bind(this));
-        keyboard_el.addEventListener('mouseup', this.handleMouseUp.bind(this));
-
-        keyboard_el.addEventListener
-    }
-
-    handleTouchStart(event) {
-        event.preventDefault();
-        this.touchPositions = { x: [], y: [], t: [] };
-        const t = this.t_start = Date.now();
-        this.appendTouchPositions(event.touches, t);
-
-        mouseeeConfig = {
-            showTrail: true,
-        };
-    }
-
-    handleTouchMove(event) {
-        event.preventDefault();
-        const t = Date.now();
-        this.appendTouchPositions(event.touches, t);
-    }
-
-    handleTouchEnd(event) {
-        event.preventDefault();
-        const t = Date.now();
-        this.appendTouchPositions(event.changedTouches, t);
-
-        const swipeEvent = new CustomEvent('swipe', { detail: this.touchPositions });
-        this.dispatchEvent(swipeEvent);
-
-        // mouseeeConfig = {
-        //     showTrail: false,
-        // };
-    }
-
-    handleMouseDown(event) {
-        event.preventDefault();
-        this.touchPositions = { x: [], y: [], t: [] };
-        const t = this.t_start = Date.now();
-        this.appendTouchPositions([{ clientX: event.clientX, clientY: event.clientY }], t);
-
-        mouseeeConfig = {
-            showTrail: true,
-        };
-    }
-
-    handleMouseMove(event) {
-        event.preventDefault();
-        const t = Date.now();
-        this.appendTouchPositions([{ clientX: event.clientX, clientY: event.clientY }], t);
-    }
-
-    handleMouseUp(event) {
-        event.preventDefault();
-        const t = Date.now();
-        this.appendTouchPositions([{ clientX: event.clientX, clientY: event.clientY }], t);
-        
-        const swipeEvent = new CustomEvent('swipe', { detail: this.touchPositions });
-        this.dispatchEvent(swipeEvent);
-
-        // mouseeeConfig = {
-        //     showTrail: false,
-        // };
-    }
-
-    appendTouchPositions(touchList, cur_t) {
-        const x_coef = this.grid_width / this.keyboard_el.getBoundingClientRect().width;
-        const y_coef = this.grid_height / this.keyboard_el.getBoundingClientRect().height;
-        for (const touch of touchList) {
-            const x = (touch.clientX - this.keyboard_el.getBoundingClientRect().left) * x_coef;
-            const y = (touch.clientY - this.keyboard_el.getBoundingClientRect().top) * y_coef;
-            const t = cur_t - this.t_start;
-            this.touchPositions.x.push(Math.round(x));
-            this.touchPositions.y.push(Math.round(y));
-            this.touchPositions.t.push(t);
-        }
-    }
-}
-
-
 function updatePredictions(predictions) {
     predictions.forEach((value, index) => {
         document.getElementById('pred-' + index).innerText = value;
@@ -196,10 +104,10 @@ keyboardEl.addEventListener('mousedown', clearPredictions);
 const grid_name = "extra";
 
 fill_keyboard(keyboardEl, gnameToGrid[grid_name]);
-const swipeEmiter = new SwipeEmiter(
+const swipeEmitter = new SwipeEmitter(
     keyboardEl, gnameToGrid[grid_name]["width"], gnameToGrid[grid_name]["height"]
 );
-swipeEmiter.addEventListener('swipe', handleSwipe);
+swipeEmitter.addEventListener('swipe', handleSwipe);
 
 // fill keyboard each time div size changes
 new ResizeObserver(() => {
---
+++ static/swipe_emitter.js
@@ -0,0 +1,91 @@
+export class SwipeEmitter extends EventTarget{
+    constructor(keyboard_el, width, height) {
+        super();
+        this.keyboard_el = keyboard_el;
+        this.grid_width = width
+        this.grid_height = height
+        this.touchPositions = { x: [], y: [], t: [] };
+
+        keyboard_el.addEventListener('touchstart', this.handleTouchStart.bind(this));
+        keyboard_el.addEventListener('touchmove', this.handleTouchMove.bind(this));
+        keyboard_el.addEventListener('touchend', this.handleTouchEnd.bind(this));
+        keyboard_el.addEventListener('mousedown', this.handleMouseDown.bind(this));
+        keyboard_el.addEventListener('mousemove', this.handleMouseMove.bind(this));
+        keyboard_el.addEventListener('mouseup', this.handleMouseUp.bind(this));
+
+        keyboard_el.addEventListener
+    }
+
+    handleTouchStart(event) {
+        event.preventDefault();
+        this.touchPositions = { x: [], y: [], t: [] };
+        const t = this.t_start = Date.now();
+        this.appendTouchPositions(event.touches, t);
+
+        mouseeeConfig = {
+            showTrail: true,
+        };
+    }
+
+    handleTouchMove(event) {
+        event.preventDefault();
+        const t = Date.now();
+        this.appendTouchPositions(event.touches, t);
+    }
+
+    handleTouchEnd(event) {
+        event.preventDefault();
+        const t = Date.now();
+        this.appendTouchPositions(event.changedTouches, t);
+
+        const swipeEvent = new CustomEvent('swipe', { detail: this.touchPositions });
+        this.dispatchEvent(swipeEvent);
+
+        // mouseeeConfig = {
+        //     showTrail: false,
+        // };
+    }
+
+    handleMouseDown(event) {
+        event.preventDefault();
+        this.touchPositions = { x: [], y: [], t: [] };
+        const t = this.t_start = Date.now();
+        this.appendTouchPositions([{ clientX: event.clientX, clientY: event.clientY }], t);
+
+        mouseeeConfig = {
+            showTrail: true,
+        };
+    }
+
+    handleMouseMove(event) {
+        event.preventDefault();
+        const t = Date.now();
+        this.appendTouchPositions([{ clientX: event.clientX, clientY: event.clientY }], t);
+    }
+
+    handleMouseUp(event) {
+        event.preventDefault();
+        const t = Date.now();
+        this.appendTouchPositions([{ clientX: event.clientX, clientY: event.clientY }], t);
+        
+        const swipeEvent = new CustomEvent('swipe', { detail: this.touchPositions });
+        this.dispatchEvent(swipeEvent);
+
+        // mouseeeConfig = {
+        //     showTrail: false,
+        // };
+    }
+
+    appendTouchPositions(touchList, cur_t) {
+        const x_coef = this.grid_width / this.keyboard_el.getBoundingClientRect().width;
+        const y_coef = this.grid_height / this.keyboard_el.getBoundingClientRect().height;
+        for (const touch of touchList) {
+            const x = (touch.clientX - this.keyboard_el.getBoundingClientRect().left) * x_coef;
+            const y = (touch.clientY - this.keyboard_el.getBoundingClientRect().top) * y_coef;
+            const t = cur_t - this.t_start;
+            this.touchPositions.x.push(Math.round(x));
+            this.touchPositions.y.push(Math.round(y));
+            this.touchPositions.t.push(t);
+        }
+    }
+}
